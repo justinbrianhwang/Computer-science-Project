3d69c5fc56269827609ab0c2beabe5f59e5e07b7
sj hwang <150766879+justinbrianhwang@users.noreply.github.com>
2024-09-16 17:48:22 +0900

    Update asm.bat

--- Project/Assembly Project/setting/asm.bat
+++ Project/Assembly Project/setting/asm.bat
@@ -3,5 +3,5 @@ REM asm.bat - batch file for assemble & link assembly programs
 @echo off
 ml /coff /c /Zi /Fl %1.asm 
 if errorLevel 1 goto terminate
-link %1.odj user32.lib kernel32 /subsystem:console /debug
+link %1.obj user32.lib kernel32 /subsystem:console /debug
 : terminate
