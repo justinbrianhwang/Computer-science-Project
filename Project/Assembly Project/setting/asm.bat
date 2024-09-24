80fdd1b7a54cf62d79425f39fef732491ba49dab
sj hwang <150766879+justinbrianhwang@users.noreply.github.com>
2024-09-24 23:37:14 +0900

    Delete Project/Assembly Project/setting/asm.bat

--- Project/Assembly Project/setting/asm.bat
+++
@@ -1,7 +0,0 @@
-REM asm.bat - batch file for assemble & link assembly programs
-
-@echo off
-ml /coff /c /Zi /Fl %1.asm 
-if errorLevel 1 goto terminate
-link %1.obj user32.lib kernel32 /subsystem:console /debug
-: terminate
